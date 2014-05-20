0bfede41197548760545c3915f409f6fc72421d8
Elonix Thonarel <spider0quoll@gmail.com>
2014-05-19 23:28:48 -0500

    Add forward button function

--- Orientation_build01/src/com/orientation_build01/MainActivity.java
+++ Orientation_build01/src/com/orientation_build01/MainActivity.java
@@ -18,8 +18,11 @@ package com.orientation_build01;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
@@ -27,21 +30,36 @@ public class MainActivity extends Activity {
 
 	private GLSurfaceView mGLView;
 	private LinearLayout container;
+	private Button forwardButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//request full screen
+		// request full screen
+		requestNoTitleFullScreen();
+		
+		setContentView(R.layout.activity_main);
+		findViews();
+	}
+
+	private void findViews() {
+		// Create a GLSurfaceView instance and set it
+				// as the ContentView for this Activity.
+		mGLView = new MyGLSurfaceView(this);
+		forwardButton = (Button) findViewById(R.id.button_forward);
+		forwardButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				((MyGLSurfaceView) mGLView).moveForward();
+			}});
+		container = (LinearLayout) findViewById(R.id.container);
+		container.addView(mGLView);
+	}
+
+	private void requestNoTitleFullScreen() {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-		// Create a GLSurfaceView instance and set it
-		// as the ContentView for this Activity.
-		mGLView = new MyGLSurfaceView(this);
-		setContentView(R.layout.activity_main);
-		container = (LinearLayout) findViewById(R.id.container);
-		container.addView(mGLView);
 	}
 
 	@Override
--- Orientation_build01/src/com/orientation_build01/MyGLSurfaceView.java
+++ Orientation_build01/src/com/orientation_build01/MyGLSurfaceView.java
@@ -20,78 +20,92 @@ import android.opengl.GLSurfaceView;
 import android.view.MotionEvent;
 
 /**
- * A view container where OpenGL ES graphics can be drawn on screen.
- * This view can also be used to capture touch events, such as a user
- * interacting with drawn objects.
+ * A view container where OpenGL ES graphics can be drawn on screen. This view
+ * can also be used to capture touch events, such as a user interacting with
+ * drawn objects.
  */
 public class MyGLSurfaceView extends GLSurfaceView {
 
-    private final MyGLRenderer mRenderer;
+	private final MyGLRenderer mRenderer;
 
-    public MyGLSurfaceView(Context context) {
-        super(context);
+	public MyGLSurfaceView(Context context) {
+		super(context);
 
-        // Set the Renderer for drawing on the GLSurfaceView
-        mRenderer = new MyGLRenderer(context);
-        setRenderer(mRenderer);
+		// Set the Renderer for drawing on the GLSurfaceView
+		mRenderer = new MyGLRenderer(context);
+		setRenderer(mRenderer);
 
-        // Render the view only when there is a change in the drawing data
-        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-    }
+		// Render the view only when there is a change in the drawing data
+		setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+	}
 
-    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
-    private float mPreviousX;
-    private float mPreviousY;
-    private boolean touch=false;
-    private double step=0.1*Maze.Size;
+	private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+	private float mPreviousX;
+	private float mPreviousY;
+	private boolean touch = false;
+	private double step = 0.1 * Maze.Size;
 
-    @Override
-    public boolean onTouchEvent(MotionEvent e) {
-        // MotionEvent reports input details from the touch screen
-        // and other input controls. In this case, we are only
-        // interested in events where the touch position changed.
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		// MotionEvent reports input details from the touch screen
+		// and other input controls. In this case, we are only
+		// interested in events where the touch position changed.
 
-        float x = e.getX();
-        float y = e.getY();
+		float x = e.getX();
+		float y = e.getY();
 
-        switch (e.getAction()) {
-        	case MotionEvent.ACTION_DOWN:
-        		touch=true;
-        	break;
-            case MotionEvent.ACTION_MOVE:
+		switch (e.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			touch = true;
+			break;
+		case MotionEvent.ACTION_MOVE:
 
-                float dx = x - mPreviousX;
-                float dy = y - mPreviousY;
+			float dx = x - mPreviousX;
+			float dy = y - mPreviousY;
 
-//                // reverse direction of rotation above the mid-line
-//                if (y > getHeight() / 2) {
-//                    dx = dx * -1 ;
-//                }
-//
-//                // reverse direction of rotation to left of the mid-line
-//                if (x < getWidth() / 2) {
-//                    dy = dy * -1 ;
-//                }
+			// // reverse direction of rotation above the mid-line
+			// if (y > getHeight() / 2) {
+			// dx = dx * -1 ;
+			// }
+			//
+			// // reverse direction of rotation to left of the mid-line
+			// if (x < getWidth() / 2) {
+			// dy = dy * -1 ;
+			// }
 
-                mRenderer.setAngle(
-                        mRenderer.getAngle() +
-                        ((dx + dy) * TOUCH_SCALE_FACTOR));  // = 180.0f / 320
-                touch=false;
-                requestRender();
-                break;
-            case MotionEvent.ACTION_UP:
-            	if (touch==true){
-            	mRenderer.setXYZ((float)(mRenderer.getX()+step*Math.cos(-mRenderer.getAngle()/180.0f*Math.PI)), 
-            			(float)(mRenderer.getY()+step*Math.sin(-mRenderer.getAngle()/180.0f*Math.PI)),
-            			mRenderer.getZ());
-            	requestRender();
-            	}
-            	break;
-            	
-        }
+			mRenderer.setAngle(mRenderer.getAngle()
+					+ ((dx + dy) * TOUCH_SCALE_FACTOR)); // = 180.0f / 320
+			touch = false;
+			requestRender();
+			break;
+		case MotionEvent.ACTION_UP:
+			if (touch == true) {
+				mRenderer.setXYZ(
+						(float) (mRenderer.getX() + step
+								* Math.cos(-mRenderer.getAngle() / 180.0f
+										* Math.PI)),
+						(float) (mRenderer.getY() + step
+								* Math.sin(-mRenderer.getAngle() / 180.0f
+										* Math.PI)), mRenderer.getZ());
+				requestRender();
+			}
+			break;
 
-        mPreviousX = x;
-        mPreviousY = y;
-        return true;
-    }
+		}
+
+		mPreviousX = x;
+		mPreviousY = y;
+		return true;
+	}
+	
+	public void moveForward(){
+		mRenderer.setXYZ(
+				(float) (mRenderer.getX() + step
+						* Math.cos(-mRenderer.getAngle() / 180.0f
+								* Math.PI)),
+				(float) (mRenderer.getY() + step
+						* Math.sin(-mRenderer.getAngle() / 180.0f
+								* Math.PI)), mRenderer.getZ());
+		requestRender();
+	}
 }
